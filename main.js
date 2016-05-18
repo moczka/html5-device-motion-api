e9724805bb3b16ef2a0b31b8c1b883ba94e752a7
moczka <steven.guesp@gmail.com>
2016-05-18 17:50:20 -0400

    Changed the event listener for device motion. Added an arrow to visually display the orientation angle with respect to the canvas plane.

--- main.js
+++ main.js
@@ -7,24 +7,47 @@ function onWindowLoad(){
 function motionApp(){
 	if(window.DeviceOrientationEvent){
 		//browser supports device orientation. add listneer
-	window.addEventListener('deviceorientation', devOrientationHandler, false);
+	window.addEventListener('devicemotion', devMotionHandler, false);
 	}
 	
 	var tiltLR;
 	var tiltFB;
+	var ax;
+	var ay;
 	var dir;
 	
+	var productAngle = 0;
+	var arrowSprite = new Image();
+	arrowSprite.src = 'assets/sprites/arrow.png';
 	var gammaOutput = $('#gammaOutput');
 	var betaOutput = $('#betaOutput');
+	var mainCanvas = $('#mainCanvas');
+	var mainContext = mainCanvas.getContext('2d');
 	
-	function devOrientationHandler(e){
+	
+	function devMotionHandler(e){
 		//handles the device orientation event.
-		tiltLR = e.gamma;
-		tiltFB = e.beta;
-		dir = e.alpha;
+		tiltLR = Math.floor(e.rotationRate.gamma);
+		tiltFB = Math.floor(e.rotationRate.beta);
+		dir = e.rotationRate.alpha;
+		
+		ax = e.acceleration.x;
+		ay = e.acceleration.y;
+		
+		productAngle = Math.atan2(ay, ax);
 		
 		gammaOutput.innerHTML = "Device Left/Right Tilt: "+tiltLR;
 		betaOutput.innerHTML = "Device Front/Back Tilt: "+tiltFB;
+		
+		
+		//draws the canvas with arrow
+		mainContext.clearRect(0,0,mainCanvas.width, mainCanvas.height);
+		
+		mainContext.save();
+		mainContext.translate(mainCanvas.width/2-100, mainCanvas.height/2)
+		mainContext.rotate(productAngle);
+		mainContext.drawImage(arrowSprite, 0, 0);
+		mainContext.restore();
 		
 	}
 	//end of motion app
