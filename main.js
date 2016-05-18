7874a8283e99bfb64f4abcd5a59f00f68c0dfd2f
moczka <steven.guesp@gmail.com>
2016-05-18 18:35:06 -0400

    A sprite was added to illutrate the device motion

--- main.js
+++ main.js
@@ -17,37 +17,52 @@ function motionApp(){
 	var dir;
 	
 	var productAngle = 0;
-	var arrowSprite = new Image();
-	arrowSprite.src = 'assets/sprites/arrow.png';
+	var shipSprite = new Image();
+	shipSprite.src = 'assets/sprites/ship.png';
 	var gammaOutput = $('#gammaOutput');
 	var betaOutput = $('#betaOutput');
 	var mainCanvas = $('#mainCanvas');
 	var mainContext = mainCanvas.getContext('2d');
 	
+	var ship = {x:425, y:240, velX:0, velY:0, thrust:0.25, width:35, height:28, angle:0};
+	
+	
+	setInterval(function(){
+				//draws the canvas with ship
+		mainContext.fillStyle = '#000';
+		mainContext.fillRect(0,0,mainCanvas.width, mainCanvas.height);
+		
+		
+		ship.x += ship.velX;
+		ship.y += ship.velX;
+		
+		mainContext.save();
+		mainContext.translate(ship.x, ship.y);
+		mainContext.rotate(ship.angle);
+		mainContext.drawImage(shipSprite, 0-ship.width/2, 0-ship.height/2);
+		mainContext.restore();
+	
+		
+	}, 25);
+	
+	
 	
 	function devMotionHandler(e){
 		//handles the device orientation event.
-		tiltLR = Math.floor(e.rotationRate.gamma);
-		tiltFB = Math.floor(e.rotationRate.beta);
+		tiltLR = e.rotationRate.gamma;
+		tiltFB = e.rotationRate.beta;
 		dir = e.rotationRate.alpha;
 		
-		ax = e.acceleration.x;
-		ay = e.acceleration.y;
+		ax = e.acceleration.x * ship.thrust;
+		ay = e.acceleration.y * ship.thrust;
 		
-		productAngle = Math.atan2(ay, ax);
+		ship.velX += ax;
+		ship.velY += ay;
+		
+		ship.angle = Math.atan2(ship.velY, ship.velX);
 		
 		gammaOutput.innerHTML = "Device Left/Right Tilt: "+tiltLR;
 		betaOutput.innerHTML = "Device Front/Back Tilt: "+tiltFB;
-		
-		
-		//draws the canvas with arrow
-		mainContext.clearRect(0,0,mainCanvas.width, mainCanvas.height);
-		
-		mainContext.save();
-		mainContext.translate(mainCanvas.width/2-100, mainCanvas.height/2)
-		mainContext.rotate(productAngle);
-		mainContext.drawImage(arrowSprite, 0, 0);
-		mainContext.restore();
 		
 	}
 	//end of motion app
